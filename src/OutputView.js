8d028f37d96d98eef067d61d8996cf59144eeca4
sejineeee <sejinee0918@gmail.com>
2023-11-12 11:33:23 +0900

    fix: export default 설정 및 Console import

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,11 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printMenu() {
+    Console.print('<주문 메뉴>');
     // ...
-}
+  },
+  // ...
+};
+
+export default OutputView;
