b13978091566a8f3523af17078529632cb83d8d9
sejineeee <sejinee0918@gmail.com>
2023-11-13 17:05:38 +0900

    feature: 방문날짜 입력 및 유효성 검사 기능 추가

---
+++ src/ReservationDate.js
@@ -0,0 +1,21 @@
+class ReservationDate {
+  #date;
+
+  constructor(date) {
+    this.validate(Number(date));
+
+    this.#date = Number(date);
+  }
+
+  validate(date) {
+    if (Number.isNaN(date) || date === 0 || !Number.isInteger(date)) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
+
+    if (1 > date || 31 < date) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
+  }
+}
+
+export default ReservationDate;
