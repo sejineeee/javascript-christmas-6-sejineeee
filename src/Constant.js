0706407ce8b6be28ff10f82eaf9d485b685fe725
sejineeee <sejinee0918@gmail.com>
2023-11-14 15:55:44 +0900

    feature: 메뉴이름 상수 추가

---
+++ src/Constant.js
@@ -0,0 +1,9 @@
+export const APPETIZER_MENUS = ['양송이수프', '타파스', '시저샐러드'];
+export const MAIN_MENUS = [
+  '티본스테이크',
+  '바비큐립',
+  '해산물파스타',
+  '크리스마스파스타',
+];
+export const DESSERT_MENUS = ['초코케이크', '아이스크림'];
+export const BEVERAGE_MENUS = ['제로콜라', '레드와인', '샴페인'];
