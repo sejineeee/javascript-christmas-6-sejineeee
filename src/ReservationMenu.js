2ff80e4f9985f38e9771577036d97123695e35f4
sejineeee <sejinee0918@gmail.com>
2023-11-14 10:34:38 +0900

    feature: 아무것도 입력하지 않았을 경우 유효성 검사 기능 추가

--- src/ReservationMenu.js
+++ src/ReservationMenu.js
@@ -4,7 +4,14 @@ class ReservationMenu {
   #menuList;
 
   constructor(menuList) {
+    this.validate(menuList);
     this.#menuList = this.splitMenuData(menuList);
+  }
+
+  validate(menuList) {
+    if (menuList === '') {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
   }
 
   splitMenuData(menuList) {
