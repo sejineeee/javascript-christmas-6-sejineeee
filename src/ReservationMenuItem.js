342215ba472c11e66bbab00b645eed39d60c576b
sejineeee <sejinee0918@gmail.com>
2023-11-14 15:01:42 +0900

    feature: menu, number 값에 접근할 수 있도록 메서드 추가

--- src/ReservationMenuItem.js
+++ src/ReservationMenuItem.js
@@ -39,6 +39,14 @@ class ReservationMenuItem {
       throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
   }
+
+  getMenu() {
+    return this.#menu;
+  }
+
+  getNumber() {
+    return this.#number;
+  }
 }
 
 export default ReservationMenuItem;
