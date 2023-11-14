6785417efd1d1bddd3b0f58f1f696f0c9770832c
sejineeee <sejinee0918@gmail.com>
2023-11-15 02:04:41 +0900

    feature: 할인 전 총주문금액 계산하는 함수 추가

--- src/DecemberPromotion.js
+++ src/DecemberPromotion.js
@@ -6,6 +6,19 @@ class DecemberPromotion {
     this.#reservationDate = date;
     this.#reservationMenu = menu;
   }
+
+  calculateTotalAmountBeforeDiscount() {
+    const totalList = this.#reservationMenu.map((menuItem) =>
+      menuItem.calculate(),
+    );
+
+    const totalAmount = totalList.reduce(
+      (acc, currentValue) => acc + currentValue,
+      0,
+    );
+
+    return totalAmount;
+  }
 }
 
 export default DecemberPromotion;
