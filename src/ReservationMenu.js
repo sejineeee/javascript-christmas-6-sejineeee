0ff16cfbe0448fc03ecec68dd8a3ebe8ad1fb00c
sejineeee <sejinee0918@gmail.com>
2023-11-15 00:57:21 +0900

    fix: Error 문구 수정

--- src/ReservationMenu.js
+++ src/ReservationMenu.js
@@ -12,7 +12,7 @@ class ReservationMenu {
 
   validate(menuList) {
     if (menuList === '') {
-      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
   }
 
