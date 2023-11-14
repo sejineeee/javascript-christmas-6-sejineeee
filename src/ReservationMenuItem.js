eff090f198d1290d102e5ff08cdca9641adc050c
sejineeee <sejinee0918@gmail.com>
2023-11-14 15:56:15 +0900

    refactor: 상수 제거, 상수 파일 import 추가

--- src/ReservationMenuItem.js
+++ src/ReservationMenuItem.js
@@ -1,12 +1,9 @@
-const APPETIZER_MENUS = ['양송이수프', '타파스', '시저샐러드'];
-const MAIN_MENUS = [
-  '티본스테이크',
-  '바비큐립',
-  '해산물파스타',
-  '크리스마스파스타',
-];
-const DESSERT_MENUS = ['초코케이크', '아이스크림'];
-const BEVERAGE_MENUS = ['제로콜라', '레드와인', '샴페인'];
+import {
+  APPETIZER_MENUS,
+  MAIN_MENUS,
+  DESSERT_MENUS,
+  BEVERAGE_MENUS,
+} from './Constant.js';
 
 class ReservationMenuItem {
   #menu;
