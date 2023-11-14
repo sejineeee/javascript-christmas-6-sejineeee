d7a8d74fd7ff9f4bc1ffe02306eb2d30a81faca7
sejineeee <sejinee0918@gmail.com>
2023-11-15 02:03:09 +0900

    feature: DecemberPromotion 클래스 생성

---
+++ src/DecemberPromotion.js
@@ -0,0 +1,11 @@
+class DecemberPromotion {
+  #reservationDate;
+  #reservationMenu;
+
+  constructor(date, menu) {
+    this.#reservationDate = date;
+    this.#reservationMenu = menu;
+  }
+}
+
+export default DecemberPromotion;
