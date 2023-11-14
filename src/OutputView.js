1dd09cd3b08480036babda29e8b2bbe1cee223d0
sejineeee <sejinee0918@gmail.com>
2023-11-14 15:44:11 +0900

    feature: 주문 메뉴 출력 로직 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printMenu(menuList) {
+    Console.print(
+      '12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n<주문 메뉴>',
+    );
+
+    menuList.forEach((menuItem) => {
+      Console.print(`${menuItem.getMenu()} ${menuItem.getNumber()}개\n`);
+    });
   },
-  // ...
 };
 
 export default OutputView;
