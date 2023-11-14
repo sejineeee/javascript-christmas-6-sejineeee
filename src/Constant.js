4fa9d53d11423710682703c853698ff062e9af98
sejineeee <sejinee0918@gmail.com>
2023-11-14 17:37:29 +0900

    fix: 메뉴이름 배열에서 메뉴명과 가격을 담은 객체로 변경

--- src/Constant.js
+++ src/Constant.js
@@ -1,9 +1,23 @@
-export const APPETIZER_MENUS = ['양송이수프', '타파스', '시저샐러드'];
-export const MAIN_MENUS = [
-  '티본스테이크',
-  '바비큐립',
-  '해산물파스타',
-  '크리스마스파스타',
-];
-export const DESSERT_MENUS = ['초코케이크', '아이스크림'];
-export const BEVERAGE_MENUS = ['제로콜라', '레드와인', '샴페인'];
+export const APPETIZER_MENUS = {
+  양송이수프: 6000,
+  타파스: 5500,
+  시저샐러드: 8000,
+};
+
+export const MAIN_MENUS = {
+  티본스테이크: 55000,
+  바비큐립: 54000,
+  해산물파스타: 35000,
+  크리스마스파스타: 25000,
+};
+
+export const DESSERT_MENUS = {
+  초코케이크: 15000,
+  아이스크림: 5000,
+};
+
+export const BEVERAGE_MENUS = {
+  제로콜라: 3000,
+  레드와인: 60000,
+  샴페인: 25000,
+};
