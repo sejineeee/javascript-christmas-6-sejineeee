9b56fda0c848d8f56e14384f3c41ac700e84d5e4
sejineeee <sejinee0918@gmail.com>
2023-11-14 15:02:08 +0900

    feature: menu, number 유효성 검사 기능 추가

--- src/ReservationMenu.js
+++ src/ReservationMenu.js
@@ -1,11 +1,15 @@
 import ReservationMenuItem from './ReservationMenuItem.js';
 
+const MAX_NUMBER = 20;
+
 class ReservationMenu {
   #menuList;
 
   constructor(menuList) {
     this.validate(menuList);
     this.#menuList = this.splitMenuData(menuList);
+    this.validateDuplicateMenu(this.#menuList);
+    this.validateMaxNumber(this.#menuList);
   }
 
   validate(menuList) {
@@ -22,6 +26,31 @@ class ReservationMenu {
     });
 
     return splitMenuData;
+  }
+
+  validateDuplicateMenu(menuList) {
+    const menuNameList = new Set();
+    menuList.forEach((menuItem) => {
+      if (menuNameList.has(menuItem.getMenu())) {
+        throw new Error(
+          '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+        );
+      }
+      menuNameList.add(menuItem.getMenu());
+    });
+  }
+
+  validateMaxNumber(menuList) {
+    const InitialCount = 0;
+    const count = menuList.reduce((acc, currentValue) => {
+      return acc + currentValue.getNumber();
+    }, InitialCount);
+
+    if (count > MAX_NUMBER) {
+      throw new Error(
+        '[ERROR] 메뉴는 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.',
+      );
+    }
   }
 }
 
