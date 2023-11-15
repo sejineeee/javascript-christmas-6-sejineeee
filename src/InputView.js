4e0651b1b8f2c025d57630654dcc2ade31822593
sejineeee <sejinee0918@gmail.com>
2023-11-15 17:52:44 +0900

    fix: 개행문자 추가

--- src/InputView.js
+++ src/InputView.js
@@ -3,7 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 const InputView = {
   async readDate() {
     const input = await Console.readLineAsync(
-      '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+      '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
     );
 
     return input;
@@ -11,7 +11,7 @@ const InputView = {
 
   async readMenu() {
     const input = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
     );
 
     return input;
